ef225f1418af874d53085ce81274bb0f60ed4248
abercrombieHALO <78283697+abercrombieHALO@users.noreply.github.com>
2024-03-18 12:28:57 +0000

    Hide update bar on print

--- scripts/team_list_export_excel.js
+++ scripts/team_list_export_excel.js
@@ -135,6 +135,6 @@ function printPage() {
 	var strDate = (new Date()).toLocaleDateString("en-GB", dateOptions);
 	title = title + " - Date exported: " + strDate;
 	$("#printTitle").text(title);
-	
+	$("#updateBar").hide();
 	window.print();
 }
